6490e30ac0c3fa650615f07c36c7baea8a5af9ac
Andrey Urusov <drobus@gmail.com>
2013-12-29 18:10:12 +0400

    tabs -> spaces

--- src/main.cpp
+++ src/main.cpp
@@ -11,49 +11,49 @@
 
 int main()
 {
-	typedef boost::accumulators::accumulator_set<
-		double,
-		boost::accumulators::stats<
-			boost::accumulators::tag::weighted_mean,
-			boost::accumulators::tag::min,
-			boost::accumulators::tag::max,
-			boost::accumulators::tag::count,
-			boost::accumulators::tag::weighted_variance,
-			boost::accumulators::tag::weighted_median(boost::accumulators::with_p_square_quantile)
-		>,
-		double
-	> acc_type;
+    typedef boost::accumulators::accumulator_set<
+        double,
+        boost::accumulators::stats <
+            boost::accumulators::tag::weighted_mean,
+            boost::accumulators::tag::min,
+            boost::accumulators::tag::max,
+            boost::accumulators::tag::count,
+            boost::accumulators::tag::weighted_variance,
+            boost::accumulators::tag::weighted_median(boost::accumulators::with_p_square_quantile)
+        >,
+        double
+    > acc_type;
 
-	acc_type acc;
-	acc(0, boost::accumulators::weight = 7.000000);
-	acc(1, boost::accumulators::weight = 7.000000);
-	acc(2, boost::accumulators::weight = 3.000000);
-	acc(1, boost::accumulators::weight = 4.000000);
-	acc(2, boost::accumulators::weight = 6.000000);
-	acc(1, boost::accumulators::weight = 1.000000);
-	acc(2, boost::accumulators::weight = 7.000000);
-	acc(3, boost::accumulators::weight = 2.000000);
-	acc(2, boost::accumulators::weight = 5.000000);
-	acc(3, boost::accumulators::weight = 5.000000);
-	acc(2, boost::accumulators::weight = 2.000000);
-	acc(3, boost::accumulators::weight = 7.000000);
-	acc(4, boost::accumulators::weight = 1.000000);
-	acc(3, boost::accumulators::weight = 6.000000);
-	acc(4, boost::accumulators::weight = 4.000000);
-	acc(3, boost::accumulators::weight = 3.000000);
-	acc(4, boost::accumulators::weight = 7.000000);
-	acc(5, boost::accumulators::weight = 0.000000);
-	acc(4, boost::accumulators::weight = 7.000000);
-	acc(5, boost::accumulators::weight = 3.000000);
-	acc(4, boost::accumulators::weight = 4.000000);
-	acc(5, boost::accumulators::weight = 6.000000);
-	acc(4, boost::accumulators::weight = 1.000000);
-	acc(5, boost::accumulators::weight = 7.000000);
-	acc(6, boost::accumulators::weight = 2.000000);
-	acc(5, boost::accumulators::weight = 0.000000);
+    acc_type acc;
+    acc(0, boost::accumulators::weight = 7.000000);
+    acc(1, boost::accumulators::weight = 7.000000);
+    acc(2, boost::accumulators::weight = 3.000000);
+    acc(1, boost::accumulators::weight = 4.000000);
+    acc(2, boost::accumulators::weight = 6.000000);
+    acc(1, boost::accumulators::weight = 1.000000);
+    acc(2, boost::accumulators::weight = 7.000000);
+    acc(3, boost::accumulators::weight = 2.000000);
+    acc(2, boost::accumulators::weight = 5.000000);
+    acc(3, boost::accumulators::weight = 5.000000);
+    acc(2, boost::accumulators::weight = 2.000000);
+    acc(3, boost::accumulators::weight = 7.000000);
+    acc(4, boost::accumulators::weight = 1.000000);
+    acc(3, boost::accumulators::weight = 6.000000);
+    acc(4, boost::accumulators::weight = 4.000000);
+    acc(3, boost::accumulators::weight = 3.000000);
+    acc(4, boost::accumulators::weight = 7.000000);
+    acc(5, boost::accumulators::weight = 0.000000);
+    acc(4, boost::accumulators::weight = 7.000000);
+    acc(5, boost::accumulators::weight = 3.000000);
+    acc(4, boost::accumulators::weight = 4.000000);
+    acc(5, boost::accumulators::weight = 6.000000);
+    acc(4, boost::accumulators::weight = 1.000000);
+    acc(5, boost::accumulators::weight = 7.000000);
+    acc(6, boost::accumulators::weight = 2.000000);
+    acc(5, boost::accumulators::weight = 0.000000);
 
-	double median = boost::accumulators::weighted_median(acc);
-	std::cout << median << std::endl;
+    double median = boost::accumulators::weighted_median(acc);
+    std::cout << median << std::endl;
 
     return 0;
 }
